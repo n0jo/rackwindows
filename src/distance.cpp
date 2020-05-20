da265af9ec13b57eb14cf0db0198aac5281e60d7
Jens Robert Janke <jensrobertjanke@gmail.com>
2020-05-20 21:50:18 +0300

    Add polyphony

--- src/distance.cpp
+++ src/distance.cpp
@@ -8,6 +8,7 @@ Ported and designed by Jens Robert Janke
 Changes/Additions:
 - mono
 - CV inputs for distance and dry/wet
+- polyphonic
 
 Some UI elements based on graphics from the Component Library by Wes Milholen. 
 
@@ -36,15 +37,17 @@ struct Distance : Module {
         NUM_LIGHTS
     };
 
-    double lastclampL;
-    double clampL;
-    double changeL;
-    double thirdresultL;
-    double prevresultL;
-    double lastL;
+    const double gainCut = 0.03125;
+    const double gainBoost = 32.0;
 
-    long double fpNShapeL;
-    //default stuff
+    double lastclamp[16];
+    double clasp[16];
+    double change[16];
+    double thirdresult[16];
+    double prevresult[16];
+    double last[16];
+
+    long double fpNShape[16];
 
     float A;
     float B;
@@ -55,11 +58,13 @@ struct Distance : Module {
         configParam(DISTANCE_PARAM, 0.f, 1.f, 0.f, "Distance");
         configParam(DRYWET_PARAM, 0.f, 1.f, 1.f, "Dry/Wet");
 
+        for (int i = 0; i < 16; i++) {
+            thirdresult[i] = prevresult[i] = lastclamp[i] = clasp[i] = change[i] = last[i] = 0.0;
+            fpNShape[i] = 0.0;
+        }
+
         A = 0.0;
         B = 1.0;
-        thirdresultL = prevresultL = lastclampL = clampL = changeL = lastL = 0.0;
-        fpNShapeL = 0.0;
-        //this is reset: values being initialized only once. Startup values, whatever they are.
     }
 
     void process(const ProcessArgs& args) override
@@ -75,15 +80,6 @@ struct Distance : Module {
             B += inputs[DRYWET_CV_INPUT].getVoltage() / 5;
             B = clamp(B, 0.01f, 0.99f);
 
-            double gainCut = 0.0000152587890625;
-            double gainBoost = 65536.0;
-
-            // input
-            float in1 = inputs[IN_INPUT].getVoltage();
-
-            // pad gain massively to prevent distortion
-            in1 *= gainCut;
-
             double overallscale = 1.0;
             overallscale /= 44100.0;
             overallscale *= args.sampleRate;
@@ -98,75 +94,89 @@ struct Distance : Module {
             double dry = 1.0 - wet;
             double bridgerectifier;
 
-            long double inputSampleL;
+            long double inputSample;
             long double drySampleL;
 
-            inputSampleL = in1;
-            if (inputSampleL < 1.2e-38 && -inputSampleL < 1.2e-38) {
-                static int noisesource = 0;
-                //this declares a variable before anything else is compiled. It won't keep assigning
-                //it to 0 for every sample, it's as if the declaration doesn't exist in this context,
-                //but it lets me add this denormalization fix in a single place rather than updating
-                //it in three different locations. The variable isn't thread-safe but this is only
-                //a random seed and we can share it with whatever.
-                noisesource = noisesource % 1700021;
-                noisesource++;
-                int residue = noisesource * noisesource;
-                residue = residue % 170003;
-                residue *= residue;
-                residue = residue % 17011;
-                residue *= residue;
-                residue = residue % 1709;
-                residue *= residue;
-                residue = residue % 173;
-                residue *= residue;
-                residue = residue % 17;
-                double applyresidue = residue;
-                applyresidue *= 0.00000001;
-                applyresidue *= 0.00000001;
-                inputSampleL = applyresidue;
+            // number of polyphonic channels
+            int numChannels = inputs[IN_INPUT].getChannels();
+
+            // for each poly channel
+            for (int i = 0; i < numChannels; i++) {
+
+                // input
+                inputSample = inputs[IN_INPUT].getPolyVoltage(i);
+
+                // pad gain
+                inputSample *= gainCut;
+
+                if (inputSample < 1.2e-38 && -inputSample < 1.2e-38) {
+                    static int noisesource = 0;
+                    //this declares a variable before anything else is compiled. It won't keep assigning
+                    //it to 0 for every sample, it's as if the declaration doesn't exist in this context,
+                    //but it lets me add this denormalization fix in a single place rather than updating
+                    //it in three different locations. The variable isn't thread-safe but this is only
+                    //a random seed and we can share it with whatever.
+                    noisesource = noisesource % 1700021;
+                    noisesource++;
+                    int residue = noisesource * noisesource;
+                    residue = residue % 170003;
+                    residue *= residue;
+                    residue = residue % 17011;
+                    residue *= residue;
+                    residue = residue % 1709;
+                    residue *= residue;
+                    residue = residue % 173;
+                    residue *= residue;
+                    residue = residue % 17;
+                    double applyresidue = residue;
+                    applyresidue *= 0.00000001;
+                    applyresidue *= 0.00000001;
+                    inputSample = applyresidue;
+                }
+
+                drySampleL = inputSample;
+
+                inputSample *= softslew;
+                lastclamp[i] = clasp[i];
+                clasp[i] = inputSample - last[i];
+                postfilter = change[i] = fabs(clasp[i] - lastclamp[i]);
+                postfilter += filtercorrect;
+                if (change[i] > 1.5707963267949)
+                    change[i] = 1.5707963267949;
+                bridgerectifier = (1.0 - sin(change[i]));
+                if (bridgerectifier < 0.0)
+                    bridgerectifier = 0.0;
+                inputSample = last[i] + (clasp[i] * bridgerectifier);
+                last[i] = inputSample;
+                inputSample /= softslew;
+                inputSample += (thirdresult[i] * thirdfilter);
+                inputSample /= (thirdfilter + 1.0);
+                inputSample += (prevresult[i] * postfilter);
+                inputSample /= (postfilter + 1.0);
+                //do an IIR like thing to further squish superdistant stuff
+                thirdresult[i] = prevresult[i];
+                prevresult[i] = inputSample;
+                inputSample *= levelcorrect;
+
+                if (wet < 1.0) {
+                    inputSample = (drySampleL * dry) + (inputSample * wet);
+                }
+
+                //stereo 32 bit dither, made small and tidy.
+                int expon;
+                frexpf((float)inputSample, &expon);
+                long double dither = (rand() / (RAND_MAX * 7.737125245533627e+25)) * pow(2, expon + 62);
+                inputSample += (dither - fpNShape[i]);
+                fpNShape[i] = dither;
+                //end 32 bit dither
+
+                // bring gain back up
+                inputSample *= gainBoost;
+
+                // output
+                outputs[OUT_OUTPUT].setChannels(numChannels);
+                outputs[OUT_OUTPUT].setVoltage(inputSample, i);
             }
-            drySampleL = inputSampleL;
-
-            inputSampleL *= softslew;
-            lastclampL = clampL;
-            clampL = inputSampleL - lastL;
-            postfilter = changeL = fabs(clampL - lastclampL);
-            postfilter += filtercorrect;
-            if (changeL > 1.5707963267949)
-                changeL = 1.5707963267949;
-            bridgerectifier = (1.0 - sin(changeL));
-            if (bridgerectifier < 0.0)
-                bridgerectifier = 0.0;
-            inputSampleL = lastL + (clampL * bridgerectifier);
-            lastL = inputSampleL;
-            inputSampleL /= softslew;
-            inputSampleL += (thirdresultL * thirdfilter);
-            inputSampleL /= (thirdfilter + 1.0);
-            inputSampleL += (prevresultL * postfilter);
-            inputSampleL /= (postfilter + 1.0);
-            //do an IIR like thing to further squish superdistant stuff
-            thirdresultL = prevresultL;
-            prevresultL = inputSampleL;
-            inputSampleL *= levelcorrect;
-
-            if (wet < 1.0) {
-                inputSampleL = (drySampleL * dry) + (inputSampleL * wet);
-            }
-
-            //stereo 32 bit dither, made small and tidy.
-            int expon;
-            frexpf((float)inputSampleL, &expon);
-            long double dither = (rand() / (RAND_MAX * 7.737125245533627e+25)) * pow(2, expon + 62);
-            inputSampleL += (dither - fpNShapeL);
-            fpNShapeL = dither;
-            //end 32 bit dither
-
-            // bring gain back up
-            inputSampleL *= gainBoost;
-
-            // output
-            outputs[OUT_OUTPUT].setVoltage(inputSampleL);
         }
     }
 };
