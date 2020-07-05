ead4b4c4134955196d9515dbac257ca75fedcf6a
Jens Robert Janke <jensrobertjanke@gmail.com>
2020-07-05 11:59:11 +0300

    Adjust param update frequency

--- src/mv.cpp
+++ src/mv.cpp
@@ -218,7 +218,7 @@ struct Mv : Module {
         quality = 0;
         quality = loadQuality();
 
-        partTimeJob.setDivision(64);
+        partTimeJob.setDivision(2);
 
         updateParams();
 
